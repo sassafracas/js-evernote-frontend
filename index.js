fcaf82b4512460085d55d3d97cd88f2e7a551cf3
github username <torpid@gmail.com>
2018-06-18 15:57:43 -0400

    added buttons

--- index.js
+++ index.js
@@ -17,18 +17,51 @@ document.addEventListener("DOMContentLoaded", function () {
     const h2 = document.createElement("h2")
     const div = document.getElementById('left-bar')
     const p = document.createElement("p")
+    const editButton = document.createElement("button")
+    const deleteButton = document.createElement("button")
+    const br = document.createElement("br")
+
+    editButton.setAttribute("class", "edit")
+    editButton.innerText = "Edit"
+    deleteButton.setAttribute("class", "delete")
+    deleteButton.innerText = "Delete"
 
     h2.innerText = noteObj.title
     p.innerText = noteObj.body.substring(0, 100) + "..."
-    
+
     li.appendChild(h2)
+    p.appendChild(br)
+    p.appendChild(br)
+    p.appendChild(editButton)
+    p.appendChild(deleteButton)
     li.appendChild(p)
     ul.appendChild(li)
     div.appendChild(ul)
   }
 
+  function makeNewDetailForm (){
+    const body = document.getElementById('main-body')
+    const newNoteForm = document.createElement("form")
+    const inputButton = document.createElement("input")
+    const br = document.createElement("br")
+
+
+    inputButton.setAttribute("type", "submit")
+    inputButton.setAttribute("value", "Submit")
+    newNoteForm.innerText = "New Note"
+    newNoteForm.appendChild(br)
+    newNoteForm.appendChild(inputButton)
+    body.appendChild(newNoteForm)
+    
+    newNoteForm.addEventListener("submit", function (event){
+      event.preventDefault()
+      console.log(event) //this will point to a function to create an empty details page
+    })
+  }
+
   function startApp () {
     getAllNotes()
+    makeNewDetailForm()
   }
 
   startApp()
