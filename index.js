6124934e64b806c8ba42e426882ec35ae6519aba
github username <torpid@gmail.com>
2018-06-18 21:00:30 -0400

    new form

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   const usersAPI = "http://localhost:3000/api/v1/users"
   const notesAPI = "http://localhost:3000/api/v1/notes"
-
+  const ul = document.getElementById("note-ul")
+  const div = document.getElementById('left-bar')
 
   function getAllNotes () {
     fetch(notesAPI).then(response => response.json()).then(iterateNotes)
@@ -12,10 +13,8 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function getOneNote (noteObj) {
-    const ul = document.createElement("ul")
     const li = document.createElement("li")
     const h2 = document.createElement("h2")
-    const div = document.getElementById('left-bar')
     const p = document.createElement("p")
     const editButton = document.createElement("button")
     const deleteButton = document.createElement("button")
@@ -37,6 +36,14 @@ document.addEventListener("DOMContentLoaded", function () {
     li.appendChild(p)
     ul.appendChild(li)
     div.appendChild(ul)
+
+    ul.addEventListener('click', function (event){
+      if (event.target === editButton){
+        console.log("hi")
+      } else if (event.target === deleteButton){
+        // fetch(`${notesAPI}/${noteObj.id}`, {method: "delete"}).then(response => response.json()).then(json => return json)
+      }
+    })
   }
 
   function makeNewDetailForm (){
@@ -45,18 +52,65 @@ document.addEventListener("DOMContentLoaded", function () {
     const inputButton = document.createElement("input")
     const br = document.createElement("br")
 
-
     inputButton.setAttribute("type", "submit")
     inputButton.setAttribute("value", "Submit")
     newNoteForm.innerText = "New Note"
     newNoteForm.appendChild(br)
     newNoteForm.appendChild(inputButton)
     body.appendChild(newNoteForm)
-    
+
     newNoteForm.addEventListener("submit", function (event){
       event.preventDefault()
-      console.log(event) //this will point to a function to create an empty details page
+      console.log(event)
+      newDetailDiv()
     })
+  }
+
+  function newDetailDiv () {
+    const rightDiv = document.getElementById("right-bar")
+    if (!document.getElementById('right-form')) {
+      const rightForm = document.createElement("form")
+      const rightTitle = document.createElement("input")
+      const rightDetails = document.createElement("textarea")
+      const br = document.createElement("br")
+      const br2 = document.createElement("br")
+      const br3 = document.createElement("br")
+      const br4 = document.createElement("br")
+      const br5 = document.createElement("br")
+      const newNoteButton = document.createElement("input")
+      const titleText = document.createTextNode("Note Title")
+      const descriptionText = document.createTextNode("Note Description")
+
+      newNoteButton.setAttribute("type", "submit")
+      newNoteButton.setAttribute("value", "New Note")
+      rightTitle.setAttribute("type", "text")
+      rightForm.setAttribute("id", "right-form")
+
+      rightForm.appendChild(br2)
+      rightForm.appendChild(titleText)
+      rightForm.appendChild(br5)
+      rightForm.appendChild(rightTitle)
+      rightForm.appendChild(br)
+      rightForm.appendChild(descriptionText)
+      rightForm.appendChild(br4)
+      rightForm.appendChild(rightDetails)
+      rightForm.appendChild(br3)
+      rightForm.appendChild(newNoteButton)
+      rightDiv.appendChild(rightForm)
+
+      rightDiv.addEventListener("submit", function (event) {
+        event.preventDefault()
+        //console.log(event)
+        config = {
+          headers: {
+            "Content-Type" : "application/json"
+          },
+          method: "POST",
+          body: JSON.stringify({title: event.path["0"]["0"].value, body: event.path["0"][1].value, user_id: 1})
+        }
+        fetch(notesAPI, config).then(response => response.json()).then(getOneNote)
+      })
+  }
   }
 
   function startApp () {
